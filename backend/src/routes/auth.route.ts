b6c6e2d79018cd5f3cb6c35631ddb031a68f8607
visvoker <s2089533@gmail.com>
2025-12-28 19:26:45 +0800

    rate limiting implemented

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -6,18 +6,18 @@ import {
   logout,
   updateProfile,
 } from "../controllers/auth.controller.js";
-import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
+
+router.use(arcjetProtection);
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
-router.put("update-profile", protectRoute, updateProfile);
+router.put("update-profile", updateProfile);
 
-router.get("/check", protectRoute, (req, res) =>
-  res.status(200).json(req.user)
-);
+router.get("/check", (req, res) => res.status(200).json(req.user));
 
 export default router;
