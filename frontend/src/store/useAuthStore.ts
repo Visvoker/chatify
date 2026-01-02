25218413ab0702f3d9b3a3a88c2deec080342798
visvoker <s2089533@gmail.com>
2026-01-02 15:29:32 +0800

    signUp implemented on the frontend

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -1,12 +1,47 @@
 import { create } from "zustand";
+import toast from "react-hot-toast";
 
-export const useAuthStore = create((set) => ({
-  authUser: { name: "John", _id: 123, age: 25 },
-  isLoggedIn: false,
-  isLoading: false,
+import type { AuthUser, SignupPayload } from "../types/auth";
 
-  login: () => {
-    console.log("logged in!");
-    set({ isLoggedIn: true, isLoading: true });
+import { axiosInstance } from "../lib/axios";
+import { getErrorMessage } from "../utils/error";
+
+type AuthStore = {
+  authUser: AuthUser | null;
+  isCheckingAuth: boolean;
+  isSigningUp: boolean;
+  checkAuth: () => Promise<void>;
+  signUp: (data: SignupPayload) => Promise<void>;
+};
+
+export const useAuthStore = create<AuthStore>((set) => ({
+  authUser: null,
+  isCheckingAuth: true,
+  isSigningUp: false,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("Error in authCheck:", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+  signUp: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("auth/signup", data);
+      set({ authUser: res.data });
+
+      toast.success("Account created successfully!");
+    } catch (error: unknown) {
+      toast.error(getErrorMessage(error));
+    } finally {
+      set({ isSigningUp: false });
+    }
   },
 }));
